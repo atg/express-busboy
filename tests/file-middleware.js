64c053a23716609df5b8a59d1a4a2f38e9369782
Alex Gordon <alextgordon@gmail.com>
2018-07-17 11:36:30 +0100

    Implement low-level middleware interface

---
+++ tests/file-middleware.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2015, Yahoo Inc. All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+var assert = require('assert'),
+    bb = require('../'),
+    express = require('express'),
+    fs = require('fs'),
+    request = require('request'),
+    portfinder = require('portfinder');
+
+var port;
+var app = express();
+let mw = bb.middleware({ upload: true });
+var base = 'http://127.0.0.1:';
+var setup = function(app) {
+    app._server = app.listen(port);
+    app.all('/', mw, function(req, res) {
+        res.send({
+            body: req.body,
+            files: req.files
+        });
+    });
+};
+
+describe('express-busboy: file upload', function() {
+
+    before(function(done) {
+        portfinder.getPort(function(e, p) {
+            port = p;
+            setup(app);
+            base += p;
+            done();
+        });
+    });
+
+    after(function() {
+        app._server.close();
+    });
+
+    it('should upload a file', function(done) {
+        var r = request({
+            method: 'POST',
+            url: base + '/',
+            json: true
+        }, function(err, res, d) {
+            assert.ok(d);
+            assert.ok(d.body);
+            assert.equal(d.body.foobar, 1);
+            assert.ok(d.files);
+            assert.ok(d.files['the-file']);
+            assert.ok(d.files['the-file'].file);
+            assert.ok(fs.existsSync(d.files['the-file'].file));
+            done();
+        });
+        var form = r.form();
+        form.append('foobar', 1);
+        form.append('the-file', fs.createReadStream(__filename));
+    });
+        
+    it('should upload files of the same name', function(done) {
+        var r = request({
+            method: 'POST',
+            url: base + '/',
+            json: true
+        }, function(err, res, d) {
+            assert.ok(d);
+            assert.ok(d.body);
+            assert.equal(d.body.foobar, 1);
+            assert.ok(d.files);
+            assert.ok(d.files['the-file']);
+            assert.equal(true, Array.isArray(d.files['the-file']));
+            assert.ok(d.files['the-file'][0].file);
+            assert.ok(d.files['the-file'][1].file);
+            assert.ok(d.files['the-file'][2].file);
+            assert.ok(fs.existsSync(d.files['the-file'][0].file));
+            assert.ok(fs.existsSync(d.files['the-file'][1].file));
+            assert.ok(fs.existsSync(d.files['the-file'][2].file));
+            done();
+        });
+        var form = r.form();
+        form.append('foobar', 1);
+        form.append('the-file', fs.createReadStream(__filename));
+        form.append('the-file', fs.createReadStream(__filename));
+        form.append('the-file', fs.createReadStream(__filename));
+    });
+
+});
--- tests/index.js
+++ tests/index.js
@@ -14,10 +14,22 @@ describe('express-busboy', function() {
         assert.equal(typeof bb.extend, 'function');
     });
 
+    it('should be a function', function() {
+        assert.equal(typeof bb, 'function');
+    });
+
     it('should extend', function() {
         var d = bb.extend(app, { upload: true });
         assert.equal(d, app);
     });
+
+    it('should return two middleware components', function() {
+        var m = bb({ upload: true });
+        assert.equal(m.length, 2);
+        assert(m[0]);
+        assert(m[1]);
+    });
+
     it('should not extend app twice', function() {
         var d = bb.extend(app);
         assert.equal(d, app);
